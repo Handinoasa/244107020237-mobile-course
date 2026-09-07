0aa124f6857fdb868559ee0a580ac04c087ba7f5
Handino Asa Galih R <hasagalihr@gmail.com>
2026-09-08 05:46:18 +0700

    mini asssignment

--- 01-week-1-mobile-development-ecosystem-flutter-refresh/lib/main.dart
+++ 01-week-1-mobile-development-ecosystem-flutter-refresh/lib/main.dart
@@ -12,12 +12,20 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: const Text('Profil Mahasiswa')),
         body: const Center(
-          child: Column(mainAxisSize: MainAxisSize.min, children: [
-            Icon(Icons.school, size: 72),
-            SizedBox(height: 16),
-            Text('Handino Asa Galih Romadhon', style: TextStyle(fontSize: 24)),
-            Text('Pemrograman Mobile – Minggu 1'),
-          ]),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.school, size: 72),
+              SizedBox(height: 16),
+              Text('Handino Asa Galih Romadhon', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              SizedBox(height: 8),
+              Text('NIM: 244107020237', style: TextStyle(fontSize: 16)),
+              SizedBox(height: 4),
+              Text('Kelas: TI-3E | Polinema', style: TextStyle(fontSize: 16, color: Colors.grey)),
+              SizedBox(height: 8),
+              Text('Pemrograman Mobile – Minggu 1'),
+            ],
+          ),
         ),
       ),
     );
